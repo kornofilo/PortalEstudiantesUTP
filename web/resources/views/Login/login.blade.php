5c5618839d1177d0cb8cafa119391399f608d497
Logan8192 <wsses11000@gmail.com>
2019-03-21 23:26:02 -0500

    Login - Formulario de inicio de sesión centrado.

--- web/resources/views/Login/login.blade.php
+++ web/resources/views/Login/login.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.layout-login')
 @section('content')
 
-<div class="container h-100">
-    <div class="row h-100 justify-content-center text-center">       
+<div class="container-fluid h-100 bg-light">
+    <div class="row h-100 justify-content-center align-items-center">    
         <form action="" class="">          
-            <!-- Título del Form -->
-            <h3>Portal Estudiantil</h3> 
+            <!-- Título del Form -->        
+            <h3 class="text-center">Portal Estudiantil</h3> 
             <!-- Input de Correo Universitario -->
             <div class="form-group">
                 <div class="input-group">        
@@ -16,19 +16,21 @@
                 </div>
             </div>
             <!-- Input de Contraseña -->
-            <div class="form-group">
+            <div class="form-group ">
             <input type="password" class="form-control" id="inputPassword" placeholder="Contraseña" required autofocus>
             </div>
             <!-- Botón de Iniciar Sesión -->
-            <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+            <div class="form-group text-center">
+                <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+            </div>
             
             <!-- Link de Formulario de Recuperación de Contraseña -->
-            <div class="form-group">
+            <div class="form-group text-center">
                 <a href="/password_reset">¿Olvidaste tu contraseña?</a>
             </div>
 
             <!-- Link de Formulario de Registro de Usuario -->
-            <div class="form-group">
+            <div class="form-group text-center">
                 ¿No tienes cuenta? <a href="/registro"> Registrate</a>
             </div>
         </form>
