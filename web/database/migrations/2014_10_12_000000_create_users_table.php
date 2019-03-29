5151a438d312882907033e5e204bcad5c8fb4289
Logan8192 <wsses11000@gmail.com>
2019-03-28 21:19:53 -0500

    Auth - Rol de 'studiante' por Defecto al Crear una Nueva Cuenta
    Al crear una cuenta, esta tendrá el rol de 'Estudiante' por defecto.

--- web/database/migrations/2014_10_12_000000_create_users_table.php
+++ web/database/migrations/2014_10_12_000000_create_users_table.php
@@ -26,6 +26,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('rol',20)->default('Estudiante');
         });
     }
 
