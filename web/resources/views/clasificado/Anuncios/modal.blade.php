cad057825f732603386a068784ebcdf919ef1b57
Victor Cubilla <Logan8192@users.noreply.github.com>
2019-04-16 21:21:20 -0500

    Control de aparición de botón de creación de post
    - En la secciones de eventos y bolsa de trabajos, debe aparecer el botón de creación de publicación de manera exclusiva a los usuarios con rol de administrador o moderador.
    
    Co-Authored-By: kevineliot <kevineliot@users.noreply.github.com>

--- web/resources/views/clasificado/Anuncios/modal.blade.php
+++ web/resources/views/clasificado/Anuncios/modal.blade.php
@@ -1,9 +1,24 @@
 <!--Boton modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#{{isset($id_modal)?$id_modal:modal1}}">
-    @isset($btn_nombre)
-        {{$btn_nombre}}
-    @endisset
-</button>
+
+<!-- Si la sección que me encuentro es eventos o bolsa de trabajo, y mi rol es administrador o moderador. Aparece el botón de creación de publicación -->
+@if(((\Request::is('eventos')) || (\Request::is('bolsatrabajos'))) && 
+  ((Auth::user()->roles()->value('name') == "Administrador") || (Auth::user()->roles()->value('name') == "Moderador")) )  
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#{{isset($id_modal)?$id_modal:modal1}}">
+      @isset($btn_nombre)
+          {{$btn_nombre}}
+      @endisset
+  </button>  
+@endif
+
+<!-- Si la sección que me encuentro es compra/venta o tutorías o alquiler/hospedaje. Aparece el botón de creación de publicación -->
+@if( (\Request::is('clasificado/anuncios')) || (\Request::is('clasificado/tutorias')) || (\Request::is('clasificado/alquilerhospedajes')) ) 
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#{{isset($id_modal)?$id_modal:modal1}}">
+      @isset($btn_nombre)
+          {{$btn_nombre}}
+      @endisset
+  </button>  
+@endif
+
 <!-- The Modal -->
 <div class="modal fade" id="{{isset($id_modal)?$id_modal:modal1}}">
     <div class="modal-dialog modal-lg">
